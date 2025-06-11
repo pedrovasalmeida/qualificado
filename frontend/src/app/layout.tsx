3ecc145bf8815ff5d25f888d06e197d287a37fce
HEVAPE <pedro.a.almeida@avanade.com>
2025-06-11 17:30:46 -0300

    feat(frontend): create next project with 'Soon' page

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"]
+});
+
+export const metadata: Metadata = {
+  title: "Qualificado™",
+  description: "Encontre profissionais especializados em diversas áreas, prontos para atender às suas necessidades na sua cidade",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center w-screen h-screen bg-zinc-50">
+      <h1 className="text-3xl text-zinc-900 font-bold tracking-widest">Em&nbsp;&nbsp;breve.</h1>
+    </main>
+  );
+}
