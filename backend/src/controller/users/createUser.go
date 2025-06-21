3e1c81c0f0b9090c9ca070335a496ab6d7647534
HEVAPE <pedro.a.almeida@avanade.com>
2025-06-21 12:16:54 -0300

    feat: add validation to user fields

--- backend/src/controller/users/createUser.go
+++ backend/src/controller/users/createUser.go
@@ -1,9 +1,11 @@
 package users_controller
 
 import (
-	"api/src/configuration/rest_err"
+	"api/src/configuration/validation"
 	user_model "api/src/controller/users/model"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,12 +14,18 @@ func CreateUser(c *gin.Context) {
 	var user_request user_model.Request
 
 	if err := c.ShouldBindJSON(&user_request); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()),
-		)
+		log.Printf("Error while creating user, error=%s\n", err.Error())
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
-	fmt.Println(user_request)
+
+	user_response := &user_model.Response{
+		ID: user_request.Name,
+		Email: user_request.Email,
+		Name: user_request.Name,
+	} 
+	fmt.Println(user_response)
+	c.JSON(http.StatusOK, &user_response)
 }
--- backend/src/controller/users/model/request.go
+++ backend/src/controller/users/model/request.go
@@ -1,7 +1,7 @@
 package user_model
 
 type Request struct {
-	Email string `json:"email"`
-	Password string `json:"password"`
-	Name string `json:"name"`
+	Email string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,max=32,containsany=!@#$%^&*"`
+	Name string `json:"name" binding:"required,min=3,max=50"`
 }
